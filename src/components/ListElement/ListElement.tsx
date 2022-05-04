be560a305101225d322544fd8900f5c1256ad0f2
Nikodem Kwiecinski <nikokwiatek@gmail.com>
2022-05-04 20:53:20 +0200

    Setup styles and functionality to active icon

---
+++ src/components/ListElement/ListElement.tsx
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+import {Link, useLocation} from 'react-router-dom'
+
+import {IconProps} from '../Navbar/Icons/Icons'
+
+const ListElement: React.FC<IconProps> =  ({description, id, jsx, path}) => {
+
+  const [isActive, setIsActive] = useState(false);
+  const location = useLocation();
+
+  useEffect(()=>{
+    if(path === location.pathname){
+      setIsActive(prevState => !prevState)
+    }else{
+      setIsActive(false)
+    }
+  },[location])
+
+  return (
+    <li className={`mx-auto mt-16 cursor-pointer ${isActive ? 'active' : ''}`} >
+      <Link to={`${path}`} className="block font-bold">
+        {description}
+        {jsx}
+      </Link>
+    </li>
+  )
+}
+
+export default ListElement;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,52 +1,59 @@
-import React, {useState} from 'react';
-import {Link, Outlet} from 'react-router-dom'
+import React, {useEffect, useState} from 'react';
+import {Link, Outlet, useLocation} from 'react-router-dom'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { brands, solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 import {ICONS, IconProps, USERICONS} from './Icons/Icons'
 
+import ListElement from '../ListElement/ListElement'
+
 interface Props {
-  icons?: JSX.Element
+  userLoged: boolean
 }
 
-const Navbar: React.FC<Props> = () => {
-  const [userLoged, setUserLoged] = useState(false);
-  const [activeIcon, setActiveIcon] = useState('');
-
+const Navbar: React.FC<Props> = ({userLoged}) => {
+  
+  let location = useLocation();
+  
   const icons: Array<IconProps> = [...USERICONS];
 
-  const navIcons: Array<JSX.Element> | null = userLoged ? icons.map(elem => (
-    <li key={elem.id}>
-      <Link to={`${elem.path}`}>
-        {elem.description}
-      </Link>
-      {elem.jsx}
-    </li>
+  let navIcons: Array<JSX.Element> | null | undefined = userLoged ? icons.map(elem => (
+    <ListElement 
+      description={elem.description}
+      id={elem.id}
+      jsx={elem.jsx}
+      key={elem.id}
+      path={elem.path}  
+    >
+
+    </ListElement>
   )) :
   null;
 
   const logIcon: JSX.Element = !userLoged ? 
-    <li key={ICONS[0].id} className="mx-auto">
+    <li key={ICONS[0].id} className="mx-auto mt-8">
       {ICONS[0].jsx}
-      <p>{ICONS[0].description}</p>
+      <p className="mt-2 font-bold">{ICONS[0].description}</p>
     </li> :
-    <li key={ICONS[1].id} className="mx-auto">
-    {ICONS[1].jsx}
-    <Link to={`${ICONS[1].path}`}>{ICONS[1].description}</Link>
+    <li key={ICONS[1].id} className="mx-auto mt-8">
+    <Link to={`${ICONS[1].path}`} className="block font-bold">
+      {ICONS[1].jsx}
+      {ICONS[1].description}
+    </Link>
     </li>;
 
   return (
     <>
       <nav className='flex flex-col bg-light-blue xl:w-64 lg:w-40 h-screen sticky top-0'>
-          <ul className='flex grow'>
+          <ul className='flex flex-col grow text-white xl:text-xl'>
             {logIcon}
             {navIcons}
           </ul>
-          <span className='text-white text-3xl mb-4'>
+          <span className='text-white xl:text-4xl lg:text-3xl mb-4'>
             <div className='flex justify-between bottom-4'>
-              <FontAwesomeIcon icon={brands('facebook')}  className="ml-8"/>
-              <FontAwesomeIcon icon={brands('instagram')} className="mr-8"/>
+              <FontAwesomeIcon icon={brands('facebook')}  className="xl:ml-16 lg:ml-8"/>
+              <FontAwesomeIcon icon={brands('instagram')} className="xl:mr-16 lg:mr-8"/>
             </div>
           </span>
       </nav>
