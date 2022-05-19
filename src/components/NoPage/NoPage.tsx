2f388119ff25ed4cae858d329af9b5d49785f894
Nikodem Kwiecinski <nikokwiatek@gmail.com>
2022-05-19 21:28:12 +0200

    Add NoPage view

--- src/components/NoPage/NoPage.tsx
+++ src/components/NoPage/NoPage.tsx
@@ -5,8 +5,10 @@ import Footer from '../Footer/Footer';
 const NoPage: React.FC = () => {
   return (
     <section className='right-panel'>
-      <div className='right-panel-content'>
-        Error 404
+      <div className='right-panel-content flex'>
+        <h1 className='text-dark-blue m-auto font-bold text-xl'>
+          Something went wrong 404
+        </h1>
       </div>
       <Footer></Footer>
     </section>
