f616f8279b4eaba5addc9d34ca5c73a0b7562844
nikodemkwiecinski <nikokwiatek@gmail.com>
2022-05-23 20:30:26 +0200

    Enable favicon in webpack

--- webpack/common.js
+++ webpack/common.js
@@ -28,6 +28,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path(__dirname, "..", "public", "index.html"),
+      favicon: "./src/images/favicon.svg",
     }),
   ],
 };
