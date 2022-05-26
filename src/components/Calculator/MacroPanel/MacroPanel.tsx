13d3edda1a13c13c4db5d340f28e08811fdc3b60
nikodemkwiecinski <nikokwiatek@gmail.com>
2022-05-26 12:50:30 +0200

    Margin adjustments in Macro Panel

--- src/components/Calculator/MacroPanel/MacroPanel.tsx
+++ src/components/Calculator/MacroPanel/MacroPanel.tsx
@@ -59,23 +59,23 @@ const MacroPanel: React.FC = () => {
   return (
     <form onSubmit={handleSubmit} className='shadow text-dark-blue rounded border-2 w-4/5 p-4 border-extra-light-blue border-solid 2xl:overflow-auto lg:overflow-y-scroll'>
       <h2 className='text-dark-blue w-4/5 mx-auto text-center font-bold text-2xl my-2'>Input macros by yourself</h2>
-      <label htmlFor="">
+      <label htmlFor="" className='mt-2'>
         Daily calories
         <input type="number" name="" id="" value={calories} onChange={event => setCalories(parseInt(event.target.value))}/>
       </label>
-      <label htmlFor="">
+      <label htmlFor="" className='mt-2'>
         Protein
         <input type="number" name='' id='' value={protein} onChange={event => setProtein(parseInt(event.target.value))}/>
       </label>
-      <label htmlFor="">
+      <label htmlFor="" className='mt-2'>
         Fats
         <input type="number" name='' id='' value={fat} onChange={event => setFat(parseInt(event.target.value))}/>
       </label>
-      <label htmlFor="">
+      <label htmlFor="" className='mt-2'>
         Carbs
         <input type="number" name='' id='' value={carbs} onChange={event => setCarbs(parseInt(event.target.value))}/>
       </label>
-      <label htmlFor="">
+      <label htmlFor="" className='mt-2'>
         <input type="submit" value="Submit" className='block bg-dark-blue mx-auto 2xl:h-10 h-8 2xl:w-32 w-24 mt-8 text-white font-bold rounded shadow cursor-pointer'/>
       </label>
       {errorMessage}
