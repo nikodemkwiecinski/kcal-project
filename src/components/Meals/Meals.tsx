c05600b27d8c1996843d7db90538d672a65ec679
nikodemkwiecinski <nikokwiatek@gmail.com>
2022-06-14 11:48:04 +0200

    Lift currDate to Meals component

--- src/components/Meals/Meals.tsx
+++ src/components/Meals/Meals.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 import KcalPanel from "./KcalPanel/KcalPanel";
 import Footer from "../Footer/Footer";
+import TrackingPanel from "./TrackingPanel/TrackingPanel";
 
 const Meals: React.FC = () => {
+  const newDate = new Date();
+  const [currDay, setCurrDay] = useState(
+    new Date(
+      `${newDate.getFullYear()}-${newDate.getMonth() + 1}-${newDate.getDate()}`
+    )
+  );
   return (
     <section className="right-panel">
       <div className="right-panel-content bg-grey-bg flex justify-around">
-        <KcalPanel />
-        <div className="h-half w-1/3"></div>
+        <KcalPanel currDay={currDay} setCurrDay={setCurrDay} />
+        <div className="h-half w-45% bg-white shadow-lg rounded my-auto">
+          <TrackingPanel />
+        </div>
       </div>
       <Footer></Footer>
     </section>
