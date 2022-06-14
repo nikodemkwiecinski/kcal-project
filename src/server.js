828d3bebcd01df79eeb251f9b470f466d3b9447e
nikodemkwiecinski <nikokwiatek@gmail.com>
2022-06-14 11:46:42 +0200

    Remove test endpoint

--- src/server.js
+++ src/server.js
@@ -8,10 +8,6 @@ const app = express();
 
 app.use(cors());
 
-app.get("/", (request, response) => {
-  response.json("Test connection");
-});
-
 app.get("/data", (request, response) => {
   const ingredients = request.query.ingredients;
 
